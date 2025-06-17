4564aa88ce8504509193c0dabf806cecc9534c63
Graphene101102 <vietvcg.tm2002@gmail.com>
2025-06-17 16:36:38 +0700

    MVC route controller

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes/index.route');
 const app = express();
 const port = 3000;
 
 // Static file
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse request to json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -18,13 +23,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
