a9f80860f2ef8eedeb1abdbc44912a45ad7d9545
Graphene101102 <vietvcg.tm2002@gmail.com>
2025-07-09 17:26:25 +0700

    create course

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const CourseCotroller = require('../app/controllers/CourseCotroller');
+const CourseController = require('../app/controllers/CourseController');
 const router = express.Router();
 
-// router.get('/create', CourseCotroller.create);
-router.get('/:slug', CourseCotroller.detail);
+router.get('/create', CourseController.create);
+router.post('/store', CourseController.store);
+router.get('/:slug', CourseController.detail);
 
 
 module.exports = router;
